ee5f9079ac06daef6af86cb63a84c06daf9d70a2
Adem Halilović <halilovicadem@gmail.com>
2023-05-14 13:39:50 -0700

    Dodavanje ocjena

---
+++ eStomatolog/eStomatologServices/Database/Ocjena.cs
@@ -0,0 +1,21 @@
+﻿using eStomatologModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStomatologServices.Database
+{
+    public partial class Ocjene
+    {
+        public int Id { get; set; }
+        public int PacijentId { get; set; }
+        public int DoktorId { get; set; }
+        public DateTime Datum { get; set; }
+        public int Ocjena { get; set; }
+
+        public virtual Pacijent Pacijent { get; set; } = null!;
+        public virtual Doktor Doktor { get; set; } = null!;
+    }
+}
