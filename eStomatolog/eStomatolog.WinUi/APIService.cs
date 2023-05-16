ee2687f43e1732f28024d306d1fb35ffe8537763
Adem Halilović <halilovicadem@gmail.com>
2023-05-16 14:32:10 -0700

    Dodavanje forme za korisnike i API servisa

---
+++ eStomatolog/eStomatolog.WinUi/APIService.cs
@@ -0,0 +1,32 @@
+﻿using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStomatolog.WinUi
+{
+    public class APIService
+    {
+        private string _resource = null;
+        public string _endpoint = "https://localhost:7265/";
+
+        public APIService(string resource)
+        {
+            this._resource = resource;
+        }
+
+        public async Task<T> Get<T>()
+        {
+            var list = await $"{_endpoint}{_resource}".GetJsonAsync<T>();
+            return list;
+        }
+
+        public async Task<T> GetById<T>(int id)
+        {
+            var list = await $"{_endpoint}{_resource}/{id}".GetJsonAsync<T>();
+            return list;
+        }
+    }
+}
---
+++ eStomatolog/eStomatolog.WinUi/frmPacijentiList.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace eStomatolog.WinUi
+{
+    partial class frmPacijentiList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrikazi = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrikazi
+            // 
+            this.btnPrikazi.Location = new System.Drawing.Point(713, 45);
+            this.btnPrikazi.Name = "btnPrikazi";
+            this.btnPrikazi.Size = new System.Drawing.Size(75, 23);
+            this.btnPrikazi.TabIndex = 0;
+            this.btnPrikazi.Text = "Prikazi";
+            this.btnPrikazi.UseMnemonic = false;
+            this.btnPrikazi.UseVisualStyleBackColor = true;
+            this.btnPrikazi.Click += new System.EventHandler(this.btnPrikazi_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 339);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // frmPacijentiList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 454);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnPrikazi);
+            this.Name = "frmPacijentiList";
+            this.Text = "frmPacijentiList";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnPrikazi;
+        private DataGridView dataGridView1;
+    }
+}
