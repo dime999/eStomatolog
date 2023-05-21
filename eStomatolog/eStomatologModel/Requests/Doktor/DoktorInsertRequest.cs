3d24a582e25e7cdd2a33a410bd3cbb61ebd1ade3
Adem Halilović <halilovicadem@gmail.com>
2023-05-21 12:40:24 -0700

    Dodavanje inserta

---
+++ eStomatolog/eStomatologModel/Requests/Doktor/DoktorInsertRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eStomatologModel.Requests.Korisnik;
+
+namespace eStomatologModel.Requests.Doktor
+{
+    public class DoktorInsertRequest : KorisniciInsertRequest
+    {
+    }
+}
