6af8effc721469a61df421b4fee519005cf2a6ff
Adem Halilović <halilovicadem@gmail.com>
2023-10-03 01:17:53 +0200

    Implamentacija RabbitMQ (Ispisivanje poruke nakon napravljenje rezervacije)

--- eStomatolog/eStomatologServices/Servisi/RezervacijaService.cs
+++ eStomatolog/eStomatologServices/Servisi/RezervacijaService.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
+using EasyNetQ;
 using eStomatologModel;
 using eStomatologModel.Requests;
 using eStomatologModel.SearchObjects;
 using eStomatologServices.Interfejsi;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Services.Users;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +27,36 @@ namespace eStomatologServices.Servisi
             var entity = Context.Set<eStomatologServices.Database.Rezervacija>().Include(pac => pac.Pacijent).Include(dok=> dok.Doktor).Include(ord=> ord.Ordinacija).AsQueryable();
             var list = entity.ToList();
             return Mapper.Map<IList<Rezervacija>>(list);
+        }
+
+        public override eStomatologModel.Rezervacija Insert(RezervacijaInsertRequest insert)
+        {
+         
+            var entity = base.Insert(insert);
+
+            //var factory = new ConnectionFactory { HostName = "localhost", Port = 15672, UserName = "test", Password = "test123" };
+            //using var connection = factory.CreateConnection();
+            //using var channel = connection.CreateModel();
+
+            //channel.QueueDeclare(queue: "reservation_added",
+            //                     durable: false,
+            //                     exclusive: false,
+            //                     autoDelete: false,
+            //                     arguments: null);
+
+            //const string message = "Reservation added!";
+            //var body = Encoding.UTF8.GetBytes(message);
+
+            //channel.BasicPublish(exchange: string.Empty,
+            //                     routingKey: "reservation_added",
+            //                     basicProperties: null,
+            //                     body: body);
+
+            using var bus = RabbitHutch.CreateBus("host=localhost");
+                  
+            bus.PubSub.Publish(entity);               
+            
+            return entity;
         }
 
     }
