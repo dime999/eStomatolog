21d1cd5ddd10653032800c6b45ae96aa67d1309b
Adem Halilović <halilovicadem@gmail.com>
2023-10-17 16:24:11 +0200

    Izlged ordinacija info

--- eStomatolog/UI/estomatolog_admin/lib/screens/ordinacija/ordinacija_info.dart
+++ eStomatolog/UI/estomatolog_admin/lib/screens/ordinacija/ordinacija_info.dart
@@ -20,19 +20,25 @@ class OrdinacijaDetaljiScreen extends StatefulWidget {
 }
 
 class _OrdinacijaDetaljiScreenState extends State<OrdinacijaDetaljiScreen> {
-  late Future<Ordinacija> _ordinacijaFuture;
   int currentIndex = 0;
+  late Ordinacija ordinacija;
 
   @override
   void initState() {
     super.initState();
-    _ordinacijaFuture = fetchOrdinacija(widget.ordinacijaId);
+    fetchOrdinacija(context);
   }
 
-  Future<Ordinacija> fetchOrdinacija(int ordinacijaId) async {
-    var pacijentProvider =
+  Future<Ordinacija> fetchOrdinacija(BuildContext context) async {
+    var ordinacijaProvider =
         Provider.of<OrdinacijaProvider>(context, listen: false);
-    var ordinacija = await pacijentProvider.getById(ordinacijaId);
+    ordinacija = await ordinacijaProvider.getById(widget.ordinacijaId);
+    setState(() {
+      nazivController.text = ordinacija.naziv;
+      telefonController.text = ordinacija.telefon ?? '';
+      adresaController.text = ordinacija.adresa ?? '';
+    });
+    print(nazivController.text);
     return ordinacija;
   }
 
@@ -61,157 +67,163 @@ class _OrdinacijaDetaljiScreenState extends State<OrdinacijaDetaljiScreen> {
     return fetchedOrdinacije;
   }
 
+  TextEditingController nazivController = TextEditingController();
+  TextEditingController adresaController = TextEditingController();
+  TextEditingController telefonController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Ordinacija info")),
-      body: Center(
-        child: FutureBuilder<Ordinacija>(
-          future: _ordinacijaFuture,
-          builder: (context, ordinacijaSnapshot) {
-            if (ordinacijaSnapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (ordinacijaSnapshot.hasError) {
-              return Text('Greška pri dohvatu podataka o ordinaciji.');
-            } else if (!ordinacijaSnapshot.hasData) {
-              return Text('Nema dostupnih podataka o ordinaciji.');
-            } else {
-              Ordinacija ordinacija = ordinacijaSnapshot.data!;
-              return Row(
-                children: [
-                  // Lijeva strana - Informacije o ordinaciji
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Container(
-                        padding: EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue, width: 2.0),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              ordinacija.naziv,
-                              style: TextStyle(
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue,
+        appBar: AppBar(title: Text("Ordinacija info")),
+        body: Center(
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Lični podaci pacijenta',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 16.0),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Container(
+                                padding: EdgeInsets.all(16.0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.blue, width: 2.0),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    _buildFormField('Naziv', nazivController),
+                                    SizedBox(height: 16.0),
+                                    _buildFormField('Adresa', adresaController),
+                                    SizedBox(height: 16.0),
+                                    _buildFormField(
+                                        'Telefon', telefonController),
+                                  ],
+                                ),
                               ),
                             ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              ordinacija.adresa,
-                              style: TextStyle(fontSize: 18.0),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              ordinacija.telefon,
-                              style: TextStyle(fontSize: 18.0),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              ordinacija.gradId.toString(),
-                              style: TextStyle(fontSize: 18.0),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  // Desna strana - Slike i gumb za dodavanje nove slike
-                  Expanded(
-                    child: Column(
-                      children: [
-                        FutureBuilder<List<int>>(
-                          future: fetchOrdinacijaSlike(context),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return CircularProgressIndicator();
-                            } else if (snapshot.hasError) {
-                              return Text(
-                                  'Greška prilikom dohvata ID-ova slika.');
-                            } else if (!snapshot.hasData ||
-                                snapshot.data!.isEmpty) {
-                              return Text('Nema dostupnih ID-ova slika.');
-                            } else {
-                              List<int> slikeIds = snapshot.data!;
-                              int currentIndex = 0;
+                          ),
+                          // Desna strana - Slike i gumb za dodavanje nove slike
+                          Expanded(
+                            child: Column(
+                              children: [
+                                FutureBuilder<List<int>>(
+                                  future: fetchOrdinacijaSlike(context),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return CircularProgressIndicator();
+                                    } else if (snapshot.hasError) {
+                                      return Text(
+                                          'Greška prilikom dohvata ID-ova slika.');
+                                    } else if (!snapshot.hasData ||
+                                        snapshot.data!.isEmpty) {
+                                      return Text(
+                                          'Nema dostupnih ID-ova slika.');
+                                    } else {
+                                      List<int> slikeIds = snapshot.data!;
+                                      int currentIndex = 0;
 
-                              return Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(20.0),
-                                    child: Image.network(
-                                      "https://localhost:7265/SlikaStream?slikaId=${slikeIds[currentIndex]}",
-                                      width: 400,
-                                      height: 400,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      ElevatedButton(
-                                        onPressed: () {
-                                          if (currentIndex > 0) {
-                                            setState(() {
-                                              currentIndex--; // Prikazi prethodnu sliku
-                                            });
-                                          }
-                                        },
-                                        child: Text('Prethodna slika'),
-                                      ),
-                                      SizedBox(width: 20),
-                                      ElevatedButton(
-                                        onPressed: () {
-                                          if (currentIndex <
-                                              slikeIds.length - 1) {
-                                            setState(() {
-                                              currentIndex++; // Prikazi sljedeću sliku
-                                            });
-                                          }
-                                        },
-                                        child: Text('Sljedeća slika'),
-                                      ),
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(16.0),
-                                    child: ElevatedButton(
-                                      onPressed: () async {
-                                        await _uploadImage();
-                                        // Možete otvoriti dijalog za odabir slike iz galerije ili koristiti kameru
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        padding: EdgeInsets.symmetric(
-                                          horizontal: 32.0,
-                                          vertical: 16.0,
-                                        ),
-                                      ),
-                                      child: Text(
-                                        'Dodaj novu sliku',
-                                        style: TextStyle(
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              );
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            }
-          },
-        ),
+                                      return Column(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(20.0),
+                                            child: Image.network(
+                                              "https://localhost:7265/SlikaStream?slikaId=${slikeIds[currentIndex]}",
+                                              width: 400,
+                                              height: 400,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              ElevatedButton(
+                                                onPressed: () {
+                                                  if (currentIndex > 0) {
+                                                    setState(() {
+                                                      currentIndex--; // Prikazi prethodnu sliku
+                                                    });
+                                                  }
+                                                },
+                                                child: Text('Prethodna slika'),
+                                              ),
+                                              SizedBox(width: 20),
+                                              ElevatedButton(
+                                                onPressed: () {
+                                                  if (currentIndex <
+                                                      slikeIds.length - 1) {
+                                                    setState(() {
+                                                      currentIndex++; // Prikazi sljedeću sliku
+                                                    });
+                                                  }
+                                                },
+                                                child: Text('Sljedeća slika'),
+                                              ),
+                                            ],
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(16.0),
+                                            child: ElevatedButton(
+                                              onPressed: () async {
+                                                await _uploadImage();
+                                                // Možete otvoriti dijalog za odabir slike iz galerije ili koristiti kameru
+                                              },
+                                              style: ElevatedButton.styleFrom(
+                                                padding: EdgeInsets.symmetric(
+                                                  horizontal: 32.0,
+                                                  vertical: 16.0,
+                                                ),
+                                              ),
+                                              child: Text(
+                                                'Dodaj novu sliku',
+                                                style: TextStyle(
+                                                  fontSize: 18.0,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ]))));
+  }
+
+  Widget _buildFormField(String label, TextEditingController controller,
+      {bool isObscure = false}) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label),
+          SizedBox(height: 8.0),
+          TextField(
+            controller: controller,
+            obscureText: isObscure,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            readOnly: true,
+          ),
+        ],
       ),
     );
   }
